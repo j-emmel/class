7d4985338bd3ebfda5716e9a1a658a64176d1f4f
Jacob Emmel <jdemmel94@comcast.net>
2017-12-18 22:24:29 -0700

    Project: discussion and initial solver for review

---
+++ project/support.py
@@ -0,0 +1,87 @@
+import numpy
+
+def rk_butcher_4():
+    A = numpy.array([[0,0,0,0],[.5,0,0,0],[0,.5,0,0],[0,0,1,0]])
+    b = numpy.array([1/6, 1/3, 1/3, 1/6])
+    return A, b
+
+def ode_rkexplicit(f, u0, butcher=None, tfinal=1, h=.1):
+    if butcher is None:
+        A, b = rk_butcher_4()
+    else:
+        A, b = butcher
+    c = numpy.sum(A, axis=1)
+    s = len(c)
+    u = u0.copy()
+    t = 0
+    hist = [(t,u0)]
+    while t < tfinal:
+        if tfinal - t < 1.01*h:
+            h = tfinal - t
+            tnext = tfinal
+        else:
+            tnext = t + h
+        h = min(h, tfinal - t)
+        fY = numpy.zeros((len(u0), s))
+        for i in range(s):
+            Yi = u.copy()
+            for j in range(i):
+                Yi += h * A[i,j] * fY[:,j]
+            fY[:,i] = f(t + h*c[i], Yi)
+        u += h * fY.dot(b)
+        t = tnext
+        hist.append((t, u.copy()))
+    return hist
+
+limiters = []
+def limappend(lim):
+    limiters.append(lim)
+    return lim
+
+@limappend
+def limit_zero(r):
+    return 0*r
+
+@limappend
+def limit_none(r):
+    return 0*r+1
+
+@limappend
+def limit_minmod(r):
+    return numpy.maximum(numpy.minimum(2*r, 2*(1-r)), 0)
+
+@limappend
+def limit_sin(r):
+    return numpy.where(numpy.all((0 < r, r < 1), axis=0),
+                      numpy.sin(numpy.pi*r),
+                      0)
+
+@limappend
+def limit_vl(r):
+    return numpy.maximum(4*r*(1-r), 0)
+
+@limappend
+def limit_bj(r):
+    return numpy.clip(numpy.minimum(4*r, 4*(1-r)), 0, 1)
+
+def fvsolve2system(riemann, U0, a=-1, b=1, n=20, tfinal=1, limit=limit_minmod, t_step = None, args = ()):
+    h = (b - a)/n
+    
+    if t_step is None:
+        t_step = h/10
+    
+    x = numpy.linspace(a+h/2, b-h/2, n) # Element midpoints (centroids)
+    U0x = U0(x)
+    Ushape = U0x.shape
+    idxL = numpy.arange(-1, n-1)
+    idxR = numpy.arange(1, n+1) % n
+    def rhs(t, U):
+        U = U.reshape(Ushape)
+        jump = U[:,idxR] - U[:,idxL]
+        r = numpy.zeros_like(jump)
+        numpy.divide(U - U[:,idxL], jump, out=r, where=(jump!=0))
+        g = limit(r) * jump / (2*h)
+        fluxL = riemann(U[:,idxL] + g[:,idxL] * h/2, U - g * h/2, *args)
+        return (fluxL - fluxL[:,idxR]).flatten() / h
+    hist = ode_rkexplicit(rhs, U0x.flatten(), h=t_step, tfinal=tfinal)
+    return x, [(t, U.reshape(Ushape)) for t, U in hist]
